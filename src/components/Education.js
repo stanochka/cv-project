0a3144b76002f2405a490edb2a957d844f175154
stanochka <krozzochka@gmail.com>
2021-08-12 21:38:45 +0300

    Edit some

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,15 +1,75 @@
 import React from 'react';
 
-function Education() {
-  function addEducation() {
-    console.log('new education!');
-    return (<div>new education</div>)
+class Education extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  educations: [],
+                    education: { school: '',
+                                 location: '',
+                                 startYear: '',
+                                 finishYear: '', },
+    }
+    this.addEducation = this.addEducation.bind(this);
   }
 
-  return (<div className='Education'>
-            <h2>Education</h2>
-            <button id='newEducation' onClick={addEducation}>Add</button>
-          </div>)
+  addEducation(event) {
+    event.preventDefault();
+    this.setState({ educations: this.state.educations.concat(this.state.education),
+                    education: { school: '',
+                                 startYear: '',
+                                 finishYear: '', },
+                  })
+  }
+
+  render() {
+    return (<div className='Education'>
+              <h2>Education</h2>
+              { this.state.educations.length>0 && <Overview educations={this.state.educations} />}
+              <form id='EducationForm'>
+                <label htmlFor='school'>School</label><br />
+                <input
+                    id='school'
+                    type='textarea'
+                    value={this.state.education.school}
+                    onChange={(e) => this.setState({ education: { school: e.target.value }})}
+                ></input><br />
+                <input
+                    id='startYear'
+                    type='number'
+                    min='1900'
+                    max='2021'
+                    value={this.state.education.startYear}
+                    onChange={(e) => this.setState({ education: { startYear: e.target.value }})}
+                ></input> - <input
+                                id='finishYear'
+                                type='number'
+                                min='1900'
+                                max='2021'
+                                value={this.state.education.finishYear}
+                                onChange={(e) => this.setState({ education: { finishYear: e.target.value }})}
+                            ></input><br />
+
+                <button id='addEducation' onClick={this.addEducation}>Add</button>
+              </form>
+            </div>)
+  }
+
+}
+
+
+function Overview(props) {
+  const  { educations } = props;
+  return (
+    <ul>
+      {educations.map((education, i) => {
+        return (<li key={i}>
+                  <p>{education.school}</p>
+                  <p>{education.startYear}-{education.finishYear}</p>
+                </li>
+                );
+      })}
+    </ul>
+  );
 }
 
 export default Education;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,21 @@ import General from './General';
 import Education from './Education';
 import Work from './Work';
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (<form className="Main">
-              <General />
-              <Education />
-              <Work />
-              <button type='submit'>Save</button>
-            </form>)
-  }
+function Main() {
+  const resume = {};
+  return (<div className="Main">
+            <General />
+            <Education />
+            <Work />
+            <button type='submit' onClick={(e) => {
+              e.preventDefault();
+              resume.givenName = document.querySelector('#givenName').value;
+              resume.surname = document.querySelector('#surname').value;
+              resume.phone = document.querySelector('#phone').value;
+              resume.email = document.querySelector('#email').value;
+              console.log(resume);
+            }}>Save</button>
+          </div>)
 }
 
 export default Main;
