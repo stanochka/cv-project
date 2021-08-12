be09518cdb88a1bef6cebd3e58cfbd66f751ed57
stanochka <krozzochka@gmail.com>
2021-08-12 13:27:57 +0300

    Editable input General

--- src/components/General.js
+++ src/components/General.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
-function General() {
-  return (<div>
-            <label htmlFor='givenName'>Name</label><br />
-            <input id='givenName' type='textarea' required></input><br />
-            <label htmlFor='surname'>Surname</label><br />
-            <input id='surname' type='textarea' required></input><br />
-            <label htmlFor='phone'>Phone</label><br />
-            <input id='phone' type='textarea' required></input><br />
-            <label htmlFor='email'>Email</label><br />
-            <input id='email' type='email'></input><br />
-          </div>)
+class General extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      givenName: '',
+      surname: '',
+      phone: '',
+      email: '',
+    }
+  }
+  render() {
+    return (<div>
+              <label htmlFor='givenName'>Name</label><br />
+              <input
+                  id='givenName'
+                  type='textarea'
+                  value={this.state.givenName}
+                  onChange={(e) => this.setState({ givenName: e.target.value })}
+                  required></input><br />
+              <label htmlFor='surname'>Surname</label><br />
+              <input
+                  id='surname'
+                  type='textarea'
+                  value={this.state.surname}
+                  onChange={(e) => this.setState({ surname: e.target.value })}
+                  required></input><br />
+              <label htmlFor='phone'>Phone</label><br />
+              <input
+                  id='phone'
+                  type='textarea'
+                  value={this.state.phone}
+                  onChange={(e) => this.setState({ phone: e.target.value })}
+                  ></input><br />
+              <label htmlFor='email'>Email</label><br />
+              <input
+                  id='email'
+                  type='email'
+                  value={this.state.email}
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                  required></input><br />
+            </div>)
+  }
 }
 
 export default General;
